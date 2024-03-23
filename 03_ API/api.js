964e4be3001d1c90bda1f190089e31196df48c1a
lahcenassmira <lahcenassmira@gmail.com>
2024-03-22 18:42:55 -0700

    showing data in the body

--- 03_ API/api.js
+++ 03_ API/api.js
@@ -1,9 +1,14 @@
 
-
-const apiSsi = "https://api.wheretheiss.at/v1/satellites/25544";
-async function getData(){
+const lat = document.querySelector('.lat');
+const long = document.querySelector('.long');
+const apiSsi = 'https://api.wheretheiss.at/v1/satellites/25544';
+async function getData() {
     const response = await fetch(apiSsi);
-    const data = response.json();
+    const data = await response.json();
+    const { latitude, longitude } = data;
     console.log(data);
+    lat.textContent = latitude;
+    long.textContent = longitude;
+
 }
 getData();
